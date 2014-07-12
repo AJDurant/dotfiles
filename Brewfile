439a2638363da86cbc2fa9b1fb5cfc7feb25ca63
Andy Durant <andy@ajdurant.co.uk>
2014-07-12 23:51:35 +0100

    update from mathias

---
+++ Brewfile
@@ -0,0 +1,52 @@
+# Install command-line tools using Homebrew
+# Usage: `brew bundle Brewfile`
+
+# Make sure we’re using the latest Homebrew
+update
+
+# Upgrade any already-installed formulae
+upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+install coreutils
+# Install some other useful utilities like `sponge`
+install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+install findutils
+# Install GNU `sed`, overwriting the built-in `sed`
+install gnu-sed --default-names
+# Install Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+install bash
+install bash-completion
+
+# Install wget with IRI support
+install wget --enable-iri
+
+# Install more recent versions of some OS X tools
+install homebrew/dupes/grep
+install homebrew/dupes/screen
+install homebrew/php/php55 --with-gmp
+
+# Install other useful binaries
+install ack
+install git
+install imagemagick --with-webp
+install lynx
+brew install mobile-shell
+install nmap
+install node # This installs `npm` too using the recommended installation method
+install p7zip
+install pigz
+install rename
+install tree
+install ucspi-tcp # `tcpserver` et al.
+install webkit2png
+install xpdf
+install zopfli
+
+install homebrew/versions/lua52
+
+# Remove outdated versions from the cellar
+cleanup
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,58 +1,62 @@
 #!/usr/bin/env bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo '' &>/dev/null
+    echo '' &>/dev/null;
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-	SUBL="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+	SUBL="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl";
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-    echo '' &>/dev/null
+    echo '' &>/dev/null;
 elif [[ "$OSTYPE" == "win32" ]]; then
-    echo '' &>/dev/null
+    echo '' &>/dev/null;
 elif [[ "$OSTYPE" == "freebsd"* ]]; then
-    echo '' &>/dev/null
+    echo '' &>/dev/null;
 else
-    echo '' &>/dev/null
-fi
+    echo '' &>/dev/null;
+fi;
 
-cd "$(dirname "${BASH_SOURCE}")"
-git pull origin master
+cd "$(dirname "${BASH_SOURCE}")";
+git pull origin master;
+
 function doItCygwin() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" \
-		--exclude ".osx" --exclude ".brew" -av --no-perms . ~
-	source ~/.bash_profile
-	echo "ca_directory = /usr/ssl/certs # Cygwin CA Certs directory is non standard" >> ~/.wgetrc
+		--exclude ".osx" --exclude "Brewfile" --exclude "Caskfile" -avh --no-perms . ~;
+	source ~/.bash_profile;
+	echo "ca_directory = /usr/ssl/certs # Cygwin CA Certs directory is non standard" >> ~/.wgetrc;
 }
+
 function doItMac() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" \
-		--exclude ".minttyrc" -av --no-perms . ~
-	source ~/.bash_profile
+		--exclude ".minttyrc" -avh --no-perms . ~;
+	source ~/.bash_profile;
 }
+
 function doIt() {
 	# OS specific
 	if [[ "$OSTYPE" == "darwin"* ]]; then
-		doItMac
+		doItMac;
 	elif [[ "$OSTYPE" == "cygwin" ]]; then
-		doItCygwin
+		doItCygwin;
 	else
 		rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-			--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-		source ~/.bash_profile
+			--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+		source ~/.bash_profile;
 	fi
 	if [[] -n "$SUBL" ]]; then
 		if [[ -x "$SUBL" ]]; then
-			ln -s "${SUBL}" ~/bin/subl
-		fi
-	fi
+			ln -s "${SUBL}" ~/bin/subl;
+		fi;
+	fi;
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
-unset doIt
+		doIt;
+	fi;
+fi;
+unset doIt;
